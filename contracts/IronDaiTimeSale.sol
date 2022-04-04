8fdbc55a1815fe75b119d8986be180ebd77ea804
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2022-04-04 09:09:33 -1000

    Update and rename BITNCrowdsale.sol to IronDaiTimeSale.sol

--- contracts/IronDaiTimeSale.sol
+++ contracts/IronDaiTimeSale.sol
@@ -8,7 +8,7 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 
-contract BITNCrowdsale is Ownable, Crowdsale{
+contract IronDaiTimeSale is Ownable, Crowdsale{
 
     uint private customRate;
 
