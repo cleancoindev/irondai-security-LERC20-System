b66f0e20b050d763c93e9e7fe2a30914a24f03e3
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2022-04-04 09:13:46 -1000

    Update IronDaiTimeSaleToken.sol

--- contracts/IronDaiTimeSaleToken.sol
+++ contracts/IronDaiTimeSaleToken.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.21 <0.6.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract IronDaiToken is ERC20 {
-    string public name = "Bitcoin&Company Network Test Token";
+    string public name = "IronDaiTimeSaleToken";
     string public symbol = "$IDTS";
     uint public decimals = 1;
     uint public INITIAL_SUPPLY = 999999999999000000000;
