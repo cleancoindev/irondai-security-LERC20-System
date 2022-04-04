76bf4fc2f0287c75b36bfde4e580578f8b194312
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2022-04-04 09:13:13 -1000

    Update and rename BitcoinCompanyNetworkTestToken.sol to IronDaiTimeSaleToken.sol

--- contracts/IronDaiTimeSaleToken.sol
+++ contracts/IronDaiTimeSaleToken.sol
@@ -2,11 +2,11 @@ pragma solidity >=0.4.21 <0.6.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
-contract BitcoinCompanyNetworkTestToken is ERC20 {
+contract IronDaiToken is ERC20 {
     string public name = "Bitcoin&Company Network Test Token";
-    string public symbol = "BITNT";
-    uint public decimals = 18;
-    uint public INITIAL_SUPPLY = 1200000000000000000000000;
+    string public symbol = "$IDTS";
+    uint public decimals = 1;
+    uint public INITIAL_SUPPLY = 999999999999000000000;
 
     constructor() public {
         _mint(msg.sender, INITIAL_SUPPLY);
